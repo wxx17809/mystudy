694589fff05ab13837d959cbdcfc0dbd3bb0d1fa
岁月静好 <2805047647@qq.com>
2020-05-22 16:55:30 +0800

    Jwt

--- gaq-dao/src/main/java/com/ghkj/gaqdao/mapper/ConfigureImageMapper.java
+++
@@ -1,8 +0,0 @@
-package com.ghkj.gaqdao.mapper;
-
-import com.ghkj.gaqcommons.untils.MyMapper;
-import com.ghkj.gaqentity.ConfigureImage;
-
-public interface ConfigureImageMapper extends MyMapper<ConfigureImage> {
-
-}
